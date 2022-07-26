b73f7fa1ed08dcc40bee048e6b03503d45eaef2e
Suvel Rathneswar <find.suvel@gmail.com>
2022-07-26 21:51:29 +0530

    Added Bootstrap4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      hi
+      <div class="container">
+
+      </div>
     </div>
   );
 }
